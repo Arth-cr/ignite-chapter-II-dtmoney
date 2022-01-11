63dd63dea91721fa7ed03e0a2bd6f300240c1ddc
Arth-cr <arthur.carvalho@outlook.com.br>
2022-01-10 22:38:56 -0300

    feat: chapter II finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionProvider } from './hooks/useTransactions';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionProvider>
       <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransectionModal} />
 
@@ -28,6 +29,6 @@ export function App() {
       />
 
       <Dashboard />
-    </>
+    </TransactionProvider>
   );
 }
